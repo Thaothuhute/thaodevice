0b5c3e7722c3672fd9a1a36d19b87113e1d2e42b
thaonguyen <thaodtt475@gmail.com>
2024-04-17 11:03:35 +0700

    taoo file js

---
+++ app.js
@@ -0,0 +1,21 @@
+// Define a variable
+var message = "Hello, world!";
+
+// Print the message to the console
+console.log(message);
+
+// Perform a calculation
+var x = 10;
+var y = 5;
+var sum = x + y;
+
+// Print the result
+console.log("The sum of x and y is: " + sum);
+
+// Create a function
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+// Call the function
+greet("John");
